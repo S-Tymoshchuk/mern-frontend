57c03c4617a7629e75b5bb87a422c7a88b29856c
Sergey <lobalmax@gmail.com>
2020-01-28 14:57:02 +0200

    sideBar, backDrop

--- src/shared/Navigation/MainNavigation.js
+++ src/shared/Navigation/MainNavigation.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import BackDrop from "../UIElements/BackDrop";
 
 const MainNavigation = (props) => {
-    return <MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span/>
-            <span/>
-            <span/>
-        </button>
-        <h1 className="main-navigation__title">
-            <Link to="/">Your places</Link>
-        </h1>
-        <nav>
-            ...
-        </nav>
-    </MainHeader>;
+    const [drawerIsOpen, serDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        serDrawerIsOpen(true);
+    };
+
+    const closeDrawer = () => {
+        serDrawerIsOpen(false);
+    };
+
+    return (
+        <React.Fragment>
+            {drawerIsOpen && <BackDrop onClick={closeDrawer}/>}
+            {drawerIsOpen ? <SideDrawer>
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks/>
+                </nav>
+            </SideDrawer> : null}
+            <MainHeader>
+                <button className="main-navigation__menu-btn" onClick={openDrawer}>
+                    <span/>
+                    <span/>
+                    <span/>
+                </button>
+                <h1 className="main-navigation__title">
+                    <Link to="/">Your places</Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
+                    <NavLinks/>
+                </nav>
+            </MainHeader>
+        </React.Fragment>
+    );
 };
 
 export default MainNavigation;
--- src/shared/Navigation/NavLinks.js
+++ src/shared/Navigation/NavLinks.js
@@ -1,10 +1,23 @@
 import React from "react";
-
-import './NavLinks.css';
+import {NavLink} from "react-router-dom";
+import "./NavLinks.css";
 
 
 const NavLinks = props => {
-
+    return <ul className="nav-links">
+        <li>
+            <NavLink to="/" exact>All USERS</NavLink>
+        </li>
+        <li>
+            <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+        <li>
+            <NavLink to="/places/new">ADD PLACES</NavLink>
+        </li>
+        <li>
+            <NavLink to="/auth">AUTHENTICATION</NavLink>
+        </li>
+    </ul>;
 };
 
 export default NavLinks;
