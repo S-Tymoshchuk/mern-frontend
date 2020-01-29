17e732d880494e161f62b2d158e074b05e0ced42
Sergey <lobalmax@gmail.com>
2020-01-29 10:46:19 +0200

    user Places useParams()

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom
 import Users from "./user/pages/User";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 
 const App = () => {
@@ -14,6 +15,9 @@ const App = () => {
                 <Switch>
                     <Route path="/" exact>
                         <Users/>
+                    </Route>
+                    <Route path="/:userId/places" exact>
+                        <UserPlaces/>
                     </Route>
                     <Route path="/places/new" exact>
                         <NewPlace/>
